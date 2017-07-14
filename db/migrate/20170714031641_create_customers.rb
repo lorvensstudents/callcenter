81c8987a737172f4ed3a9a573764c6f19c6312f2
Lorvens <lorvensstudents@gmail.com>
2017-07-14 09:22:47 +0530

    this is new customer blog

---
+++ db/migrate/20170714031641_create_customers.rb
@@ -0,0 +1,12 @@
+class CreateCustomers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :customers do |t|
+      t.string :name
+      t.string :phonenumber
+      t.string :address
+      t.text :discription
+
+      t.timestamps
+    end
+  end
+end
